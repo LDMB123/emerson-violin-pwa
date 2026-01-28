a64611ca597c8255f10ddb3d0e132670d2a070d0
Louis Herman <ljherman123@gmail.com>
2026-01-28 13:18:14 -0700

    Improve global readability and palette contrast

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v105';
+const CACHE_VERSION = 'v107';
 const CACHE_NAME = `panda-violin-local-${CACHE_VERSION}`;
 const APP_SHELL_URL = './index.html';
 const OFFLINE_URL = './offline.html';
